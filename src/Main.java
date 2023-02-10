2301c1e13921a9a75f7314387e9d2a2e2595aeb3
pavel shuvalov <pmshuv975@gmail.com>
2023-02-10 22:08:15 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 13676; // стоимость билета
+
+        int money = 20; // количество рублей для одной бонусной мили
+
+        int bonus = ticket / money; // кол-во бонусных миль
+
+        System.out.println("Вам начислено: " + bonus + " бонусных миль(и)");
+        
+    }
 }
